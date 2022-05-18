cabb25d2e991b3684cacbf4b94a84f6ba72f7a13
cwh595 <cwh595@ha.org.hk>
2022-05-18 11:35:11 +0800

    refactor source code

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -3,6 +3,7 @@ package com.springboot.blog.controller;
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
+import com.springboot.blog.utils.AppConstants;
 import javafx.geometry.Pos;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,9 +31,11 @@ public class PostController {
 
     @GetMapping
     public PostResponse getAllPosts(
-            @RequestParam(value="pageNo",defaultValue="0",required=false)int pageNo,
-            @RequestParam(value="pageSize",defaultValue="10",required = false) int pageSize){
-        return postService.getAllPosts(pageNo,pageSize);
+            @RequestParam(value="pageNo",defaultValue= AppConstants.DEFAULT_PAGE_NUMBER,required=false)int pageNo,
+            @RequestParam(value="pageSize",defaultValue=AppConstants.DEFAULT_PAGE_SIZE,required = false) int pageSize,
+            @RequestParam(value="sortBy",defaultValue = AppConstants.DEFAULT_SORT_BY,required = false)String sortBy,
+            @RequestParam(value="sortDir",defaultValue =AppConstants.DEFAULT_SORT_DIRECTION,required = false)String sortDir){
+        return postService.getAllPosts(pageNo,pageSize,sortBy,sortDir);
     }
 
     @GetMapping("/{id}")
