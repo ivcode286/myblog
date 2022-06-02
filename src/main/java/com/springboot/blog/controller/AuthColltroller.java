b14482c0f8bbe6d6f7c4b84be9b2df43ccdf0662
cwh595 <cwh595@ha.org.hk>
2022-06-02 18:38:02 +0800

    adding signin and signup

--- src/main/java/com/springboot/blog/controller/AuthColltroller.java
+++ src/main/java/com/springboot/blog/controller/AuthColltroller.java
@@ -1,6 +1,12 @@
 package com.springboot.blog.controller;
 
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.entity.User;
 import com.springboot.blog.payload.LoginDto;
+import com.springboot.blog.payload.SignUpDto;
+import com.springboot.blog.respository.RoleRepository;
+import com.springboot.blog.respository.UserRepository;
+import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +14,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,10 +29,41 @@ public class AuthColltroller {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/signin")
-    public ResponseEntity<String > authenticateUser(@RequestBody LoginDto loginDto){
-        Authentication authentication=authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsernameOrEmail(),loginDto.getPassword()));
+
+    public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsernameOrEmail(), loginDto.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return new ResponseEntity<>("User signed-in successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignUpDto signUpDto) {
+        if (userRepository.existsByUsername(signUpDto.getUsername())) {
+            return new ResponseEntity<>("Username is aldready taken!", HttpStatus.BAD_REQUEST);
+        }
+
+        if (userRepository.existsByEmail(signUpDto.getEmail())) {
+            return new ResponseEntity<("Enail is aldreay taken!.", HttpStatus.BAD_REQUEST);
+        }
+
+        User user = new User();
+        user.setName(signUpDto.getName());
+        user.setUsername(signUpDto.getUsername());
+        user.setEmail(signUpDto.getEmail());
+        user.setPassword(passwordEncoder.encode(signUpDto.getPassword()));
+
+        Role roles = (Role) roleRepository.findByName("ROLE_ADMIN").get();
+        user.setRoles(Collections.singleton(roles));
+
     }
 }
