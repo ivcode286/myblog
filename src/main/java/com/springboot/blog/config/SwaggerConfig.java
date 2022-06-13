53ca23e6ee1fbcb0884e03346f6793d39a0b089e
cwh595 <cwh595@ha.org.hk>
2022-06-13 09:51:37 +0800

    add swaager config

---
+++ src/main/java/com/springboot/blog/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.springboot.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Spring Boot Blog REST APIS",
+                "Spring Boot Blog REST API Documentations",
+                "1",
+                "Terms of service",
+                new Contact("Ivan", "www.contact.net", "user@gmail,com"),
+                "License",
+                "URL",
+                Collections.emptyList()
+        );
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
